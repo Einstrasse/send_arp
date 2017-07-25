77e7326220154b687a2ed4f47b53e78ebce97609
Einstrasse <hg9587@naver.com>
2017-07-25 12:53:57 -0400

    fetching my mac addr

--- spoofer.c
+++ spoofer.c
@@ -5,10 +5,12 @@
 
 #define CMD_BUF_SIZE 256
 #define IP_ADDR_BUF_SIZE 20
+#define MAC_ADDR_BUF_SIZE 25
 int main(int argc, char* argv[]) {
 	FILE* fp;
 	char cmdbuf[CMD_BUF_SIZE];
 	char my_ip_addr_str[IP_ADDR_BUF_SIZE];
+	char my_mac_addr_str[MAC_ADDR_BUF_SIZE];
 	char *ifname;
 	if (argc < 2) {
 		fprintf(stderr, "Usage: %s [Interface name] [Victim IPv4 addr]\n", argv[0]);
@@ -25,6 +27,18 @@ int main(int argc, char* argv[]) {
 		exit(EXIT_FAILURE);
 	}
 	fgets(my_ip_addr_str, sizeof(my_ip_addr_str) - 1, fp);
+	pclose(fp);
 	printf("My IPv4 addr: %s\n", my_ip_addr_str);
+
+	sprintf(cmdbuf, "/bin/bash -c \"ifconfig %s\" | grep '[ ][0-9a-zA-Z][0-9a-zA-Z]:[0-9a-zA-Z][0-9a-zA-Z]:[0-9a-zA-Z][0-9a-zA-Z]:[0-9a-zA-Z][0-9a-zA-Z]:[0-9a-zA-Z][0-9a-zA-Z]:[0-9a-zA-Z][0-9a-zA-Z]' | awk '{print $2}'", ifname);
+	printf("cmdbuf: %s\n", cmdbuf);
+	fp = popen(cmdbuf, "r");
+	if (fp == NULL) {
+		perror("Fail to fetch Mac address\n");
+		exit(EXIT_FAILURE);
+	}
+	fgets(my_mac_addr_str, sizeof(my_mac_addr_str) - 1, fp);
+	pclose(fp);
+	printf("My Mac addr: %s\n", my_mac_addr_str);
 
 }
