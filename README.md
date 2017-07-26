7a529a149c1d056a26c651839cb66681a6c65ccf
Einstrasse <hg9587@naver.com>
2017-07-26 02:08:57 -0400

    make packet

--- README.md
+++ README.md
@@ -47,4 +47,6 @@ ifconfig and route command are implemented.
 ## References
 popen example - https://stackoverflow.com/questions/646241/c-run-a-system-command-and-get-output    
 Another method to fetch mac/ip addr - http://www.cnx-software.com/2011/04/05/c-code-to-get-mac-address-and-ip-address/    
-arp spoof reply code - http://melwin-jose.blogspot.kr/2013/07/arp-reply-spoof-c-code.html
+arp spoof reply code - http://melwin-jose.blogspot.kr/2013/07/arp-reply-spoof-c-code.html    
+arp header struct - http://unix.superglobalmegacorp.com/Net2/newsrc/netinet/if_ether.h.html    
+arp hdr struct - https://stuff.mit.edu/afs/sipb/project/merakidev/arch/i386_deb50/include/net/if_arp.h    
--- spoofer.c
+++ spoofer.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <netinet/if_ether.h>
 #include "infofetcher.h"
+#include <arpa/inet.h>
 
 #define CMD_BUF_SIZE 256
 #define IP_ADDR_BUF_SIZE 20
@@ -11,10 +12,14 @@
 #define GATE_ADDR_BUF_SIZE 20
 #define PACK_BUF_SIZE 1024 * 64
 #define PCAP_ERR_BUF_SIZE 1024
+#define SEND_BUF_SIZE 512
 
 int main(int argc, char* argv[]) {
 	pcap_t *handle;
 	char opt_verbose = 0;
+	struct ether_header* eth_hdr;
+	struct ether_arp* arp_hdr;
+	u_char send_buf[SEND_BUF_SIZE];
 	char errbuf[PCAP_ERR_BUF_SIZE];
 	char my_ip_addr_str[IP_ADDR_BUF_SIZE];
 	char my_mac_addr_str[MAC_ADDR_BUF_SIZE];
@@ -80,5 +85,44 @@ int main(int argc, char* argv[]) {
 	// 	exit(EXIT_FAILURE);
 	// }
 	// printf("My default Gateway addr: %s\n", my_defgw_addr_str);
+
+	eth_hdr = (struct ether_header*)send_buf;
+	sscanf("ff:ff:ff:ff:ff:ff", "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", &eth_hdr->ether_dhost[0], &eth_hdr->ether_dhost[1], &eth_hdr->ether_dhost[2], &eth_hdr->ether_dhost[3], &eth_hdr->ether_dhost[4], &eth_hdr->ether_dhost[5]);
+	if (opt_verbose) {
+		printf("PACKET Dst MAC addr - %02X:%02X:%02X:%02X:%02X:%02X\n", eth_hdr->ether_dhost[0], eth_hdr->ether_dhost[1], eth_hdr->ether_dhost[2], eth_hdr->ether_dhost[3], eth_hdr->ether_dhost[4], eth_hdr->ether_dhost[5]);
+	}
+
+	sscanf(my_mac_addr_str, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", &eth_hdr->ether_shost[0], &eth_hdr->ether_shost[1], &eth_hdr->ether_shost[2], &eth_hdr->ether_shost[3], &eth_hdr->ether_shost[4], &eth_hdr->ether_shost[5]);
+	if (opt_verbose) {
+		printf("PACKET Src MAC addr - %02X:%02X:%02X:%02X:%02X:%02X\n", eth_hdr->ether_shost[0], eth_hdr->ether_shost[1], eth_hdr->ether_shost[2], eth_hdr->ether_shost[3], eth_hdr->ether_shost[4], eth_hdr->ether_shost[5]);
+	}
+	eth_hdr->ether_type = ntohs(ETHERTYPE_ARP);
+	if (opt_verbose) {
+		printf("PACKET Ether type - %02X", (u_char)eth_hdr->ether_type);
+		u_char *tmp = (u_char*)&eth_hdr->ether_type;
+		tmp++;
+		printf("%02X\n",*tmp);
+	}
+	arp_hdr = (struct ether_arp*)(send_buf + sizeof(struct ether_header));
+	arp_hdr->ea_hdr.ar_hrd = ntohs(0x0001);
+	arp_hdr->ea_hdr.ar_pro = ntohs(0x0800);
+	arp_hdr->ea_hdr.ar_hln = 6;
+	arp_hdr->ea_hdr.ar_pln = 4;
+	arp_hdr->ea_hdr.ar_op = ntohs(0x0001);
+
+	sscanf(my_mac_addr_str, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", &arp_hdr->arp_sha[0], &arp_hdr->arp_sha[1], &arp_hdr->arp_sha[2], &arp_hdr->arp_sha[3], &arp_hdr->arp_sha[4], &arp_hdr->arp_sha[5]);
+	sscanf(my_ip_addr_str, "%hhd.%hhd.%hhd.%hhd", &arp_hdr->arp_spa[0], &arp_hdr->arp_spa[1], &arp_hdr->arp_spa[2], &arp_hdr->arp_spa[3]);
+	sscanf("00:00:00:00:00:00", "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", &arp_hdr->arp_tha[0], &arp_hdr->arp_tha[1], &arp_hdr->arp_tha[2], &arp_hdr->arp_tha[3], &arp_hdr->arp_tha[4], &arp_hdr->arp_tha[5]);
+	sscanf(victim_ip_addr_str, "%hhd.%hhd.%hhd.%hhd", &arp_hdr->arp_tpa[0], &arp_hdr->arp_tpa[1], &arp_hdr->arp_tpa[2], &arp_hdr->arp_tpa[3]);
+
+	if (opt_verbose) {
+		int len = sizeof(struct ether_header) + sizeof(struct ether_arp);
+		for (int i=0; i < len; i++) {
+			printf("%02X ", send_buf[i]);
+			if (i % 16 == 15) putchar('\n');
+			else if (i % 8 == 7) putchar(' ');
+		}
+		putchar('\n');
+	}
 
 }
